0f36a37986f291bc7d5e072bbf779f583a9837a3
JKFlipFlop <boazardel@gmail.com>
2020-03-15 19:24:21 +0200

    adding all up

---
+++ Exporter.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-  GetOldTweets-python
+import sys,getopt,datetime,codecs
+from collections import Counter
+if sys.version_info[0] < 3:
+    import got
+else:
+    import got3 as got
+
+def getTweets(name,query,since,until):
+	tweetCriteria = got.manager.TweetCriteria()
+	outputFileName = "output_got.csv"
+	#tweetCriteria.username = arg
+	tweetCriteria.since = "2020-02-12"
+	tweetCriteria.until = "2020-02-15"
+	tweetCriteria.querySearch = "crono"
+	tweetCriteria.topTweets = True
+	tweetCriteria.maxTweets = 10000
+	#tweetCriteria.near = '"' + arg + '"'
+	#tweetCriteria.within = '"' + arg + '"'			
+	outputFile = codecs.open(outputFileName, "w+", "utf-8")
+	outputFile.write('username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink')
+
+	print('Searching...\n')
+
+	def receiveBuffer(tweets):
+		for t in tweets:
+			outputFile.write(('\n%s;%s;%d;%d;"%s";%s;%s;%s;"%s";%s' % (t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.geo, t.mentions, t.hashtags, t.id, t.permalink)))
+		outputFile.flush()
+		print('More %d saved on file...\n' % len(tweets))
+
+	got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
+	#tweetCriteria = got.manager.TweetCriteria().setQuerySearch('europe refugees').setSince("2015-05-01").setUntil("2015-09-30").setMaxTweets(1)
+
+	outputFile.close()
+	print('Done. Output file generated "%s".' % outputFileName)
+	return outputFileName
+
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+#print(Counter(open("output_got.csv","r+", encoding="utf8").read()).most_common(100))
+
+'''
+# -*- coding: utf-8 -*-
+import sys,getopt,datetime,codecs
+if sys.version_info[0] < 3:
+    import got
+else:
+    import got3 as got
+
+def main(argv):
+
+	if len(argv) == 0:
+		print('You must pass some parameters. Use \"-h\" to help.')
+		return
+
+	if len(argv) == 1 and argv[0] == '-h':
+		f = open('exporter_help_text.txt', 'r')
+		print(f.read())
+		f.close()
+
+		return
+
+	try:
+		opts, args = getopt.getopt(argv, "", ("username=", "near=", "within=", "since=", "until=", "querysearch=", "toptweets", "maxtweets=", "output="))
+
+		tweetCriteria = got.manager.TweetCriteria()
+		outputFileName = "output_got.csv"
+
+		for opt,arg in opts:
+			if opt == '--username':
+				tweetCriteria.username = arg
+
+			elif opt == '--since':
+				tweetCriteria.since = arg
+
+			elif opt == '--until':
+				tweetCriteria.until = arg
+
+			elif opt == '--querysearch':
+				tweetCriteria.querySearch = arg
+
+			elif opt == '--toptweets':
+				tweetCriteria.topTweets = True
+
+			elif opt == '--maxtweets':
+				tweetCriteria.maxTweets = int(arg)
+			
+			elif opt == '--near':
+				tweetCriteria.near = '"' + arg + '"'
+			
+			elif opt == '--within':
+				tweetCriteria.within = '"' + arg + '"'
+
+			elif opt == '--within':
+				tweetCriteria.within = '"' + arg + '"'
+
+			elif opt == '--output':
+				outputFileName = arg
+				
+		outputFile = codecs.open(outputFileName, "w+", "utf-8")
+
+		outputFile.write('username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink')
+
+		print('Searching...\n')
+
+		def receiveBuffer(tweets):
+			for t in tweets:
+				outputFile.write(('\n%s;%s;%d;%d;"%s";%s;%s;%s;"%s";%s' % (t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.geo, t.mentions, t.hashtags, t.id, t.permalink)))
+			outputFile.flush()
+			print('More %d saved on file...\n' % len(tweets))
+
+		got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
+
+	except arg:
+		print('Arguments parser error, try -h' + arg)
+	finally:
+		outputFile.close()
+		print('Done. Output file generated "%s".' % outputFileName)
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
+'''
---
+++ main.py
@@ -0,0 +1,84 @@
+
+import torch
+import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from sklearn.preprocessing import MinMaxScaler
+from pandas.plotting import register_matplotlib_converters
+from torch import nn, optim
+import wget
+
+import Exporter
+
+def init():
+    #matplotlib inline
+    #config InlineBackend.figure_format='retina'
+
+    sns.set(style='whitegrid', palette='muted', font_scale=1.2)
+
+    HAPPY_COLORS_PALETTE = ["#01BEFE", "#FFDD00", "#FF7D00", "#FF006D", "#93D30C", "#8F00FF"]
+
+    sns.set_palette(sns.color_palette(HAPPY_COLORS_PALETTE))
+
+    rcParams['figure.figsize'] = 14, 10
+    register_matplotlib_converters()
+
+    RANDOM_SEED = 42
+    np.random.seed(RANDOM_SEED)
+    torch.manual_seed(RANDOM_SEED)
+
+def getData():
+    try:
+        f = open("time_series_19-covid-Confirmed.csv")
+        f.close()
+    except IOError:
+        wget.download("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
+
+    #ourfile = open(filename)
+    #print(ourfile.read())
+
+def analyzeData():
+    chart = pd.read_csv("time_series_19-covid-Confirmed.csv")
+    #print(chart.head())
+    
+    chart = chart.iloc[:, 4:]
+
+    daily_cases = chart.sum(axis=0)
+    daily_cases.index = pd.to_datetime(daily_cases.index)
+    plt.plot(daily_cases)
+    plt.title("Cumulative daily cases")
+    plt.show()
+
+    daily_cases = daily_cases.diff().fillna(daily_cases[0]).astype(np.int64)
+    plt.plot(daily_cases)
+    plt.title("daily cases")
+    plt.show()
+
+    return daily_cases
+
+
+def main():
+    
+    getData()
+    
+    daily_cases = analyzeData()
+    
+    Exporter.getTweets("output_got.csv","crona","2020-02-12","2020-02-15")
+    print(open("output_got.csv","r+", encoding="utf8").read())
+        
+    
+    test_data_size = int(0.3*len(daily_cases))
+    train_data = daily_cases[:-test_data_size]
+    test_data = daily_cases[-test_data_size:]
+    #train_data.shape
+
+
+
+
+if __name__ == '__main__':
+	main()
