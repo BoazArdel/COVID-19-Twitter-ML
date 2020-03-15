bb72a8c3ee368f638b7306e4d79a81eda0cf788c
JKFlipFlop <boazardel@gmail.com>
2020-03-15 20:48:53 +0200

    prediction tuning

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from torch import nn, optim
 import wget
 
 import Exporter
+import lstmModel
 
 def init():
     #matplotlib inline
@@ -31,6 +32,44 @@ def init():
     RANDOM_SEED = 42
     np.random.seed(RANDOM_SEED)
     torch.manual_seed(RANDOM_SEED)
+
+#train_model(model, X_train, y_train, X_test, y_test)
+def train_model(model, train_data, train_labels, test_data=None, test_labels=None):
+  loss_fn = torch.nn.MSELoss(reduction='sum')
+
+  optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
+  num_epochs = 60
+
+  train_hist = np.zeros(num_epochs)
+  test_hist = np.zeros(num_epochs)
+
+  for t in range(num_epochs):
+    model.reset_hidden_state()
+
+    y_pred = model(train_data)
+
+    loss = loss_fn(y_pred.float(), train_labels)
+
+    if test_data is not None:
+      with torch.no_grad():
+        y_test_pred = model(test_data)
+        test_loss = loss_fn(y_test_pred.float(), test_labels)
+      test_hist[t] = test_loss.item()
+
+      if t % 10 == 0:  
+        print(f'Epoch {t} train loss: {loss.item()} test loss: {test_loss.item()}')
+    elif t % 10 == 0:
+      print(f'Epoch {t} train loss: {loss.item()}')
+
+    train_hist[t] = loss.item()
+    
+    optimiser.zero_grad()
+
+    loss.backward()
+
+    optimiser.step()
+  
+  return model.eval(), train_hist, test_hist
 
 def getData():
     try:
@@ -83,12 +122,72 @@ def main():
         Exporter.getTweets("output_got2.csv","crono virus","2020-03-12","2020-03-15")
 
     print(open("output_got2.csv","r+", encoding="utf8").read())
-        
     
+    #######################################################################
+
     test_data_size = int(0.3*len(daily_cases))
     train_data = daily_cases[:-test_data_size]
     test_data = daily_cases[-test_data_size:]
     #train_data.shape
+    
+    scaler = MinMaxScaler() #values between 0-1
+    scaler = scaler.fit(np.expand_dims(train_data, axis=1))
+    train_data = scaler.transform(np.expand_dims(train_data, axis=1))
+    test_data = scaler.transform(np.expand_dims(test_data, axis=1))
+
+    def create_sequences(data, seq_length):
+        xs = []
+        ys = []
+
+        for i in range(len(data)-seq_length-1):
+            x = data[i:(i+seq_length)]
+            y = data[i+seq_length]
+            xs.append(x)
+            ys.append(y)
+
+        return np.array(xs), np.array(ys)
+
+    seq_length = 10
+    X_train, y_train = create_sequences(train_data, seq_length)
+    X_test, y_test = create_sequences(test_data, seq_length)
+
+    X_train = torch.from_numpy(X_train).float()
+    y_train = torch.from_numpy(y_train).float()
+
+    X_test = torch.from_numpy(X_test).float()
+    y_test = torch.from_numpy(y_test).float()
+
+    model = lstmModel.CoronaVirusPredictor(n_features=1, n_hidden=512, seq_len=seq_length, n_layers=2)
+    model, train_hist, test_hist = train_model(model, X_train, y_train, X_test, y_test)
+
+    plt.plot(train_hist, label="Training loss")
+    plt.plot(test_hist, label="Test loss")
+    plt.ylim((0, 5))
+    plt.legend()
+    plt.show()
+
+
+    with torch.no_grad():
+        test_seq = X_test[:1]
+        preds = []
+        for _ in range(len(X_test)):
+            y_test_pred = model(test_seq)
+            pred = torch.flatten(y_test_pred).item()
+            preds.append(pred)
+            new_seq = test_seq.numpy().flatten()
+            new_seq = np.append(new_seq, [pred])
+            new_seq = new_seq[1:]
+            test_seq = torch.as_tensor(new_seq).view(1, seq_length, 1).float()
+    
+    true_cases = scaler.inverse_transform(np.expand_dims(y_test.flatten().numpy(), axis=0)).flatten()
+    predicted_cases = scaler.inverse_transform(np.expand_dims(preds, axis=0)).flatten()
+
+    plt.plot(daily_cases.index[:len(train_data)], scaler.inverse_transform(train_data).flatten(),label='Historical Daily Cases')
+    plt.plot(daily_cases.index[len(train_data):len(train_data) + len(true_cases)], true_cases,label='Real Daily Cases')
+    plt.plot(daily_cases.index[len(train_data):len(train_data) + len(true_cases)], predicted_cases, label='Predicted Daily Cases')
+    plt.show()
+
+
 
 
 
